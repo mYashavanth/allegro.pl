261ff5b1647f135401a1a92f225e328d42842db1
mYashavanth <yashavantham143@gmail.com>
2024-02-03 16:09:22 +0530

    added db connection

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -2,14 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log({ msg: `MongoDB Connected` });
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.log({error: error.message});
   }
 };
 
