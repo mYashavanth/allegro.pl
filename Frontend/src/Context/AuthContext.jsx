e4ac764bbaa9ffbade60d48d77afd788d48a40d3
mYashavanth <yashavantham143@gmail.com>
2024-02-12 23:32:27 +0530

    added all routes required for front end

--- Frontend/src/Context/AuthContext.jsx
+++ Frontend/src/Context/AuthContext.jsx
@@ -4,7 +4,6 @@ export const AuthContext = React.createContext();
 
 export default function AuthContextProvider({ children }) {
   const [loggedIn, setLoggedIn] = React.useState(false);
-  const data = 'hi'
 
   return (
     <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
